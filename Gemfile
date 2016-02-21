5a9756f9b8fac47e112060891bd2f8e4edfeca48
Walter Kerr & Jonathan Huang & Michael Du <pair+WALTERKERR.jonwhuang.supermikol@devbootcamp.com>
2016-02-20 17:31:33 -0800

    Adds working upload and data parser function

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'rack-cors', :require => 'rack/cors'
-gem 'json'
 gem 'geocoder'
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'font-awesome-rails'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,13 +9,15 @@ class PagesController < ApplicationController
 
   def parse_upload
     uploaded_io = params[:tempdata]
-    obj = JSON.parse(uploaded_io.read)
-    obj.each_value do |data|
-      if data[3]["ObjectTempF"].to_f >= 85
-        a = Datum.create(temp: data[3]["ObjectTempF"].to_f, city: params[:city], state: params[:state], time: params[:tempdate][:date])
+    p file = uploaded_io.read.gsub(/[\\\"]/,"")
+    p temps = file.scan(/(?<=ObjectTempF\:)(\d+\.\d+)/).flatten
+    temps.each do |temperature|
+
+      if temperature.to_f >= 85
+        a = Datum.create(temp: temperature.to_f, city: params[:city], state: params[:state], time: params[:tempdate][:date])
       end
     end
-    redirect_to upload_path
+    redirect_to root_path
   end
 
 end
