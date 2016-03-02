821fa355a9d8f94218bfb80ad5027184e97bee34
Jonathan Huang <jonwhuang@gmail.com>
2016-03-02 10:04:58 -0800

    Reorganize all js files

--- app/assets/javascripts/charts/scatterplots.js
+++ app/assets/javascripts/charts/scatterplots.js
@@ -1,3 +1,223 @@
+var displayScatterPlotAmbientTemps = function(allTemps){
+
+  var scatterplotData = []
+  for (var i = 0; i < allTemps.length; i++) {
+
+    scatterplotData.push({allInfaredTemperatures: allTemps[i][1], allAmbientTemperatures: allTemps[i][0]});
+  }
+
+  $('#national-chart-container').highcharts({
+    chart: {
+      type: 'scatter',
+      zoomType: 'xy'
+    },
+    navigator : {
+      enabled: true,
+      adaptToUpdatedData: false,
+      series : {
+        data : allTemps
+      }
+    },
+    exporting: {
+            buttons: {
+                contextButton: {
+                    menuItems: [{
+                        text: 'Export to PNG',
+                        onclick: function () {
+                            this.exportChart();
+                        },
+                        separator: false
+                    }, {
+                        text: 'Export to PDF',
+                        onclick: function () {
+                            this.exportChart({
+                                type: 'application/pdf'
+                            });
+                        }
+                    }, {
+                        text: 'Export to SVG',
+                        onclick: function () {
+                            this.exportChart({
+                                type: 'image/svg+xml'
+                            });
+                        }
+                    }]
+                }
+            }
+        },
+    title: {
+      text: 'National Infared Temperature Readings'
+    },
+    subtitle: {
+      text: null,
+    },
+    xAxis: {
+      title: {
+        enabled: true,
+        text: 'Ambient Temperatures'
+      },
+      startOnTick: true,
+      endOnTick: true,
+      showLastLabel: true
+    },
+    yAxis: {
+      title: {
+        text: 'Human Temperatures Measured Through Infrared'
+      }
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'left',
+      verticalAlign: 'top',
+      x: 100,
+      y: 70,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+      borderWidth: 1
+    },
+    plotOptions: {
+      scatter: {
+        marker: {
+          radius: 5,
+          states: {
+            hover: {
+              enabled: true,
+              lineColor: 'rgb(100,100,100)'
+            }
+          }
+
+        },
+        states: {
+          hover: {
+            marker: {
+              enabled: false
+            }
+          }
+        },
+        tooltip: {
+          headerFormat: '<b>{series.name}</b><br>',
+          pointFormat: '{point.y} human degrees F, {point.y} ambient degrees F'
+        }
+      }
+    },
+    series: [{
+      name: 'Human Temperature Readings',
+      color: 'rgba(223, 83, 83, .5)',
+      data: allTemps
+    }],
+  });
+}
+
+var displayScatterPlotHumidities = function(allDataPoints){
+
+  var scatterplotData = []
+  for (var i = 0; i < allDataPoints.length; i++) {
+
+    scatterplotData.push({allInfaredTemperatures: allDataPoints[i][1], allHumidities: allDataPoints[i][0]});
+  }
+
+  $('#national-chart-container').highcharts({
+    chart: {
+      type: 'scatter',
+      zoomType: 'xy'
+    },
+    navigator : {
+      enabled: true,
+      adaptToUpdatedData: false,
+      series : {
+        data : allDataPoints
+      }
+    },
+    exporting: {
+            buttons: {
+                contextButton: {
+                    menuItems: [{
+                        text: 'Export to PNG',
+                        onclick: function () {
+                            this.exportChart();
+                        },
+                        separator: false
+                    }, {
+                        text: 'Export to PDF',
+                        onclick: function () {
+                            this.exportChart({
+                                type: 'application/pdf'
+                            });
+                        }
+                    }, {
+                        text: 'Export to SVG',
+                        onclick: function () {
+                            this.exportChart({
+                                type: 'image/svg+xml'
+                            });
+                        }
+                    }]
+                }
+            }
+        },
+    title: {
+      text: 'National Infared Temperature Readings'
+    },
+    subtitle: {
+      text: 'TAP'
+    },
+    xAxis: {
+      title: {
+        enabled: true,
+        text: 'Humidity Percentage at Time of Reading'
+      },
+      startOnTick: true,
+      endOnTick: true,
+      showLastLabel: true
+    },
+    yAxis: {
+      title: {
+        text: 'Human Temperatures Measured Through Infrared'
+      }
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'left',
+      verticalAlign: 'top',
+      x: 100,
+      y: 70,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+      borderWidth: 1
+    },
+    plotOptions: {
+      scatter: {
+        marker: {
+          radius: 5,
+          states: {
+            hover: {
+              enabled: true,
+              lineColor: 'rgb(100,100,100)'
+            }
+          }
+
+        },
+        states: {
+          hover: {
+            marker: {
+              enabled: false
+            }
+          }
+        },
+        tooltip: {
+          headerFormat: '<b>{series.name}</b><br>',
+          pointFormat: '{point.x} percent humidity, {point.y} degrees F'
+        }
+      }
+    },
+    series: [{
+      name: 'Human Temperature Readings',
+      color: 'rgba(223, 83, 83, .5)',
+      data: allDataPoints
+    }]
+  });
+}
+
 var displayScatterPlotAmbientTempsSidebar = function(allTemps){
 
   var scatterplotData = []
